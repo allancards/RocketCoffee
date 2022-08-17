6e12e2a7e6cfd51a8c0ceb13b3c4ea0dce473b3f
allancards <allancardspositivo@gmail.com>
2022-08-17 09:03:57 -0300

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = document.querySelector('.menu-toggle')
+
+
+menuToggle.addEventListener("click", () =>{
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
